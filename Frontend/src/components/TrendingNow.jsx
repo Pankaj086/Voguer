41524b354f79d1cdb7a3a616fc1c8e39884529af
Pankaj086 <pankajkumar086420@gmail.com>
2025-02-11 15:20:11 +0530

    Added Trending Now Section and active property on menu in cas of small screen

--- Frontend/src/components/TrendingNow.jsx
+++ Frontend/src/components/TrendingNow.jsx
@@ -3,25 +3,26 @@ import { products } from "../assets/frontend_assets/assets";
 import Heading from "./Heading";
 import ProductCard from "./ProductCard";
 
-const NewCollection = () => {
+const TrendingNow = () => {
     
-    const [latestProducts, setLatestProducts] = useState([]);
+    const [trendingProducts, setTrendingProducts] = useState([]);
 
     useEffect(()=>{
-        setLatestProducts(products.slice(15,25));
+        const tempProducts = products.filter((product)=>(product.bestseller===true))
+        setTrendingProducts(tempProducts.slice(0,5));
     },[])
 
     return (
         <div className="my-10">
             <div className="text-center py-8 text-3xl">
-                <Heading text1={"NEW"} text2={"ARRIVALS"} />
-                <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-                    Discover our collection of new arrivals featuring the hottest styles, top trends, and exclusive pieces.
+                <Heading text1={"TRENDING"} text2={"NOW"} />
+                <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">
+                    Stay ahead of the trends. These items are currently the talk of the town and are flying off the shelves.
                 </p>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 gap-y-6 place-items-center">
                 {
-                latestProducts.map((product, index) => (
+                trendingProducts.map((product, index) => (
                     <ProductCard key={index} product={product}/>
                 ))}
             </div>
@@ -29,4 +30,4 @@ const NewCollection = () => {
     );
 };
 
-export default NewCollection;
+export default TrendingNow;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero"
 import NewCollection from "../components/NewCollection"
+import TrendingNow from "../components/TrendingNow"
 const Home = () => {
     return (
         <div>
             <Hero/>
             <NewCollection/>
+            <TrendingNow/>
         </div>
     )
 }
