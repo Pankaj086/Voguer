5a0716749f5750d0b40c5c363edd35a4fa36b856
Pankaj086 <pankajkumar086420@gmail.com>
2025-02-13 17:14:49 +0530

    Add Filter Component responsiveness

--- Frontend/src/components/Filter.jsx
+++ Frontend/src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import { CirclePlus } from 'lucide-react';
 import { useState } from 'react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
 const Dropdown = ({ title, options, open, toggleOpen }) => {
     return (
@@ -24,31 +25,73 @@ const Dropdown = ({ title, options, open, toggleOpen }) => {
 };
 
 const Filter = () => {
+    const [dropDown, setDropDown] = useState(false);
     const [categoryOpen, setCategoryOpen] = useState(true);
     const [typeOpen, setTypeOpen] = useState(true);
     const [sizeOpen, setSizeOpen] = useState(true);
+    const [mobCategoryOpen, setMobCategoryOpen] = useState(false);
+    const [mobTypeOpen, setMobTypeOpen] = useState(false);
+    const [mobSizeOpen, setMobSizeOpen] = useState(false);
 
     return (
         <div className="flex flex-col gap-2">
-            <h1 className="text-xl source-sans-3 text-[#3B2C35]">SHOP BY CATEGORY</h1>
-            <Dropdown
-                title="CATEGORY"
-                options={['Men', 'Women', 'Kids']}
-                open={categoryOpen}
-                toggleOpen={() => setCategoryOpen(!categoryOpen)}
-            />
-            <Dropdown
-                title="TYPE"
-                options={['Topwear', 'Bottomwear', 'Winterwear']}
-                open={typeOpen}
-                toggleOpen={() => setTypeOpen(!typeOpen)}
-            />
-            <Dropdown
-                title="SIZE"
-                options={['S', 'M', 'L', 'XL', 'XXL']}
-                open={sizeOpen}
-                toggleOpen={() => setSizeOpen(!sizeOpen)}
-            />
+            <div className="flex gap-2">
+                <h1 className="text-xl source-sans-3 text-[#3B2C35]">SHOP BY CATEGORY</h1>
+                <div className="block sm:hidden">
+                    <ChevronDown
+                        className={`${!dropDown ? 'block' : 'hidden'} w-6 h-6`}
+                        onClick={() => setDropDown(true)}
+                    />
+                    <ChevronUp
+                        className={`${dropDown ? 'block' : 'hidden'} w-6 h-6`}
+                        onClick={() => setDropDown(false)}
+                    />
+                </div>
+            </div>
+
+            {/* Desktop view */}
+            <div className="hidden sm:block">
+                <Dropdown
+                    title="CATEGORY"
+                    options={['Men', 'Women', 'Kids']}
+                    open={categoryOpen}
+                    toggleOpen={() => setCategoryOpen(!categoryOpen)}
+                />
+                <Dropdown
+                    title="TYPE"
+                    options={['Topwear', 'Bottomwear', 'Winterwear']}
+                    open={typeOpen}
+                    toggleOpen={() => setTypeOpen(!typeOpen)}
+                />
+                <Dropdown
+                    title="SIZE"
+                    options={['S', 'M', 'L', 'XL', 'XXL']}
+                    open={sizeOpen}
+                    toggleOpen={() => setSizeOpen(!sizeOpen)}
+                />
+            </div>
+
+            {/* Mobile view */}
+            <div className={`${dropDown ? 'block' : 'hidden'} sm:hidden`}>
+                <Dropdown
+                    title="CATEGORY"
+                    options={['Men', 'Women', 'Kids']}
+                    open={mobCategoryOpen}
+                    toggleOpen={() => setMobCategoryOpen(!mobCategoryOpen)}
+                />
+                <Dropdown
+                    title="TYPE"
+                    options={['Topwear', 'Bottomwear', 'Winterwear']}
+                    open={mobTypeOpen}
+                    toggleOpen={() => setMobTypeOpen(!mobTypeOpen)}
+                />
+                <Dropdown
+                    title="SIZE"
+                    options={['S', 'M', 'L', 'XL', 'XXL']}
+                    open={mobSizeOpen}
+                    toggleOpen={() => setMobSizeOpen(!mobSizeOpen)}
+                />
+            </div>
         </div>
     );
 };
