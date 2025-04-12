904747433fbf7955f22e67718dd9e4c3a77628b5
pankaj086 <pankajkumar086420@gmail.com>
2025-04-13 00:05:46 +0530

    Implement password hashing and JWT token generation

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -48,6 +50,50 @@ const userSchema = new mongoose.Schema({
             ref: "Order",
         }
     ],
+    refreshToken:{
+        type: String,
+    },
 },{timestamps: true});
+
+// .pre is used here to hash the password before saving it to the database
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
+
+// using bcrypt.compare method to compare the password with the hashed password in the database
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+// this function is used to generate the JWT access token for the user
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            id: this._id,
+            email: this.email,
+            name: this.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_EXPIRY,
+        },
+    )
+}
+
+// this function is used to generate the JWT refresh token for the user
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY,
+        },
+    )
+}
 
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
